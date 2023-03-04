968311e6ac595219cf221eec920e66cfaadc76fe
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-03-04 15:37:31 +0300

    fix live and soon button

--- src/components/homePage/homePage.tsx
+++ src/components/homePage/homePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import css from './homePage.module.scss'
 import {useAppSelector} from "../../hook/hook";
 import {TableOfEvents} from "../tableOfEvents/tableOfEvents";
@@ -10,6 +10,39 @@ export const HomePage: React.FC = () => {
 
     const events = useAppSelector((state) => state.events.events)
 
+    const liveEvents = events.filter(event => event.live)
+    const soonEvents = events.filter(event => !event.live)
+    const [eventsForTable, setEventsForTable] = useState<EventsItemType[]>(liveEvents)
+    const [buttonLiveIsActive, setButtonLiveIsActive] = useState(true)
+    const [buttonSoonIsActive, setButtonSoonIsActive] = useState(false)
+
+    const onClickLiveHandler = () => {
+        setEventsForTable(liveEvents)
+        setButtonLiveIsActive(true)
+        setButtonSoonIsActive(false)
+    }
+
+    const onClickSoonHandler = () => {
+        setEventsForTable(soonEvents)
+        setButtonLiveIsActive(false)
+        setButtonSoonIsActive(true)
+    }
+
+    const selectedCategory = useAppSelector(state => state.categories.items.find(el => el.name === category))
+    const eventsInThisCategory = eventsForTable.filter((event) => event.category === selectedCategory?.name)
+
+    let eventsInSelectedCategory: EventsItemType[] = []
+    if (category === 'all') {
+        eventsInSelectedCategory = eventsForTable
+    } else {
+        eventsInSelectedCategory = eventsInThisCategory
+    }
+
+
+    /*const {category} = useParams()
+
+    const events = useAppSelector((state) => state.events.events)
+
     const selectedCategory = useAppSelector(state => state.categories.items.find(el => el.name === category))
 
     const eventsInThisCategory = events.filter((event) => event.category === selectedCategory?.name)
@@ -18,7 +51,7 @@ export const HomePage: React.FC = () => {
         eventsInSelectedCategory = events
     } else {
         eventsInSelectedCategory = eventsInThisCategory
-    }
+    }*/
 
 
 
@@ -43,15 +76,18 @@ export const HomePage: React.FC = () => {
                 <button
                     type='button'
                     value='Live'
-                   /* className={buttonIsActive ? `${css.filterBar__button} ${css.activeBtn}` : css.filterBar__button}
-                    onClick={onClickLiveHandler}*/
+                    onClick={onClickLiveHandler}
+
+                    className={buttonLiveIsActive ? `${css.filterBar__button} ${css.activeBtn}` : css.filterBar__button}
+
                 >Live
                 </button>
                 <button
                     type='button'
                     value='Soon'
                      /*onClick={onClickSoonHandler}*/
-                    className={css.filterBar__button}
+                    className={buttonSoonIsActive ? `${css.filterBar__button} ${css.activeBtn}` : css.filterBar__button}
+                    onClick={onClickSoonHandler}
                 >Soon
                 </button>
             </div>
